2e61947e6a5c2a10e25fd16984453834c583576b
Onur ULUSOY <atadogan06@gmail.com>
2024-01-30 01:04:03 +0300

    Add admin functionality to API (#3)

--- tests/test_api.py
+++ tests/test_api.py
@@ -175,6 +175,33 @@ class Test_Storage(unittest.TestCase):
 
 
 
+
+    def test_add_admin(self):
+        id = "test_add_admin"
+        id_admin = "test_add_admin_admin"
+        the_admin_access_key =  AccessKey(id_admin)
+        the_admin_access_key.enable()
+        the_admin_access_key.set_is_admin(True)
+
+        
+        # Testing to access get_admins_url and expected to get 403
+        response = requests.get("http://localhost:7777"+get_admins_url, auth=HTTPBasicAuth("", id), )
+        self.assertEqual(response.status_code, 403)
+
+
+        # Adding the id as user with add_admin_url endpoint
+        data = {"key": id}
+        response = requests.post("http://localhost:7777"+add_admin_url, auth=HTTPBasicAuth("", id_admin), data=data)
+
+
+
+        # Testing to access get_admins_url and expected to get 200
+        response = requests.get("http://localhost:7777"+get_admins_url, auth=HTTPBasicAuth("", id), )
+        self.assertEqual(response.status_code, 200)
+
+
+
+
 backup = sys.argv
 sys.argv = [sys.argv[0]]
 unittest.main(exit=False)
--- upsonic_on_prem/api/urls.py
+++ upsonic_on_prem/api/urls.py
@@ -5,3 +5,5 @@ dump_url = "/dump"
 load_url = "/load"
 
 get_admins_url = "/get_admins"
+
+add_admin_url = "/add_admin"
