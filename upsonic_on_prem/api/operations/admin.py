cd213b05fdfd81fe885b0259e6fd4a837c02c24f
Onur Atakan ULUSOY <atadogan06@gmail.com>
2024-01-30 00:59:43 +0300

    Add admin functionality to API

--- upsonic_on_prem/api/operations/admin.py
+++ upsonic_on_prem/api/operations/admin.py
@@ -11,3 +11,21 @@ from flask import request
 @app.route(get_admins_url, methods=["get"])
 def get_admins():
     return jsonify(AccessKey.get_admins())
+
+
+
+
+
+
+@app.route(add_admin_url, methods=["POST"])
+def add_admin():
+    key = request.form.get("key")
+
+    user = AccessKey(key)
+    user.enable()
+
+    user.set_is_admin(True)
+
+    return jsonify(True)
+
+
