05024e3f5607ab3205afda7546465d6b1569c6b3
Onur Atakan ULUSOY <atadogan06@gmail.com>
2024-01-31 22:01:51 +0300

    Fix

--- upsonic_on_prem/dash/dash/settings.py
+++ upsonic_on_prem/dash/dash/settings.py
@@ -132,3 +132,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ["*"]
