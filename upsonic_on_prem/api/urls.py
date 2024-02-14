aafacbbc598af3c61fe13a71bb929cb947cd71f3
Gök Bora Yılmaz <bmeyen125@gmail.com>
2024-02-14 21:42:48 +0300

    Added get_dump_history API

--- upsonic_on_prem/api/urls.py
+++ upsonic_on_prem/api/urls.py
@@ -62,7 +62,10 @@ get_all_scopes_user_url = "/get_all_scopes_user"
 
 delete_scope_url = "/delete_scope"
 
+get_dump_history_url = "/get_dump_history"
+
 user_urs = [load_url, dump_url, get_read_scopes_of_me_url, get_write_scopes_of_me_url, get_document_of_scope_url,
+            get_dump_history_url,
             get_type_of_scope_url, create_document_of_scope_url, get_all_scopes_user_url, delete_scope_url]
 user_write_urls = [dump_url, create_document_of_scope_url, delete_scope_url]
-user_read_urls = [load_url, get_document_of_scope_url, get_type_of_scope_url]
+user_read_urls = [load_url, get_document_of_scope_url, get_dump_history_url, get_type_of_scope_url]
