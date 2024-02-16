5dedd0b0040b5301377cf67942cf28887be7024c
Onur Atakan ULUSOY <atadogan06@gmail.com>
2024-02-16 17:51:34 +0300

    Added favicon.ico redirect

--- upsonic_on_prem/dash/dash/urls.py
+++ upsonic_on_prem/dash/dash/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 from django.urls import path
 from django.urls import include
 from django.conf.urls.static import static
@@ -25,6 +26,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('app.url')),
     path('accounts/', include('allauth.urls')),
+    path('favicon.ico', lambda x: HttpResponseRedirect('/static/images/favicon.png')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
