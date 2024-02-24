f49b5dd18d78e7dd27c8d98c994e5a89a6831483
Onur Atakan ULUSOY <atadogan06@gmail.com>
2024-02-24 23:38:11 +0300

    Added feedback and session profiling with sentry [PATCH]

--- upsonic_on_prem/dash/app/templatetags/tags.py
+++ upsonic_on_prem/dash/app/templatetags/tags.py
@@ -9,3 +9,14 @@ register = template.Library()
 @register.simple_tag(name='debug_mode')
 def debug_mode():
     return settings.DEBUG
+
+
+@register.simple_tag(name='sentry')
+def sentry():
+
+    return settings.sentry
+
+@register.simple_tag(name='sentry_dsn')
+def sentry_dsn():
+
+    return settings.sentry_dsn
