250a3a927e190c16ad9cf64a7d69e4336eb2a9a8
Gok Bora <bmeyen125@gmail.com>
2024-02-27 19:00:22 +0000

    Added dump_requirement API

--- upsonic_on_prem/api/urls.py
+++ upsonic_on_prem/api/urls.py
@@ -98,12 +98,14 @@ get_all_scopes_name_prefix_url = "/get_all_scopes_name_prefix"
 
 create_version_url = "/create_version"
 
+dump_requirement_url = "/dump_requirement"
+
 user_urs = [load_url, dump_url, get_read_scopes_of_me_url, get_write_scopes_of_me_url, get_document_of_scope_url, get_security_analysis_of_scope_url, get_required_test_types_of_scope_url, get_mistakes_of_scope_url, get_time_complexity_of_scope_url,
             create_version_url,
             get_dump_history_url, load_specific_dump_url, get_all_scopes_name_prefix_url,
             get_type_of_scope_url, create_document_of_scope_url, create_security_analysis_of_scope_url, create_required_test_types_of_scope_url, create_mistakes_of_scope_url, create_time_complexity_of_scope_url, create_document_of_scope_url_old,
-            get_all_scopes_user_url, delete_scope_url, dump_code_url, dump_type_url, get_version_history_url, get_code_of_scope_url]
+            get_all_scopes_user_url, delete_scope_url, dump_code_url, dump_requirement_url, dump_type_url, get_version_history_url, get_code_of_scope_url]
 user_write_urls = [dump_url, create_document_of_scope_url, create_security_analysis_of_scope_url, create_required_test_types_of_scope_url, create_mistakes_of_scope_url, create_time_complexity_of_scope_url, create_document_of_scope_url_old, delete_scope_url,
-                   create_version_url, dump_code_url, dump_type_url]
+                   create_version_url, dump_code_url, dump_requirement_url, dump_type_url]
 user_read_urls = [load_url, get_document_of_scope_url, get_security_analysis_of_scope_url, get_required_test_types_of_scope_url, get_mistakes_of_scope_url, get_time_complexity_of_scope_url, get_dump_history_url, get_type_of_scope_url,
                   load_specific_dump_url, get_version_history_url, get_code_of_scope_url]
