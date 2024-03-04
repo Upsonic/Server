d8303ccb10e8b90bd9edefc6d8c16d88f2c83b79
Onur Atakaan <atadogan06@gmail.com>
2024-03-04 09:34:03 +0000

    Search system speed improved and cosine enabled

--- upsonic_on_prem/api/operations/user.py
+++ upsonic_on_prem/api/operations/user.py
@@ -231,7 +231,7 @@ def dump_python_version():
 @app.route(search_by_documentation_url, methods=["POST"])
 def search_by_documentation():
     question = request.form.get("question")
-    min_score = float(request.form.get("min_score", 500))
+    min_score = float(request.form.get("min_score", 0.5))
     how_many_result = int(request.form.get("how_many_result", 10))
 
     user = AccessKey(request.authorization.password)
