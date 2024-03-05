a428c22908fe9359f84401d4ef2eaa34e4cfc1dc
Onur Atakaan <atadogan06@gmail.com>
2024-03-05 21:08:27 +0000

    Update Modelfile with PARAMETER prefix for num_thread

--- ollama_setting.py
+++ ollama_setting.py
@@ -10,4 +10,4 @@ main_model_cpu = os.environ.get("main_model_cpu")
 if main_model_cpu != None:
     # append num_thread to the end of the Modelfile
     with open("Modelfile", "a") as f:
-        f.write(f"\nnum_thread {main_model_cpu}")
+        f.write(f"\nPARAMETER num_thread {main_model_cpu}")
