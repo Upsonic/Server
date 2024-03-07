dcdb1000135dfa68f13cb72a0ae2436d9d5a78b2
Onur Atakaan <atadogan06@gmail.com>
2024-03-07 21:07:36 +0000

    Add logging functionality and integrate with Betterstack

--- upsonic_on_prem/utils/logs.py
+++ upsonic_on_prem/utils/logs.py
@@ -1,22 +1,42 @@
 import rich
 from upsonic_on_prem.utils.configs import *
 
+
+from logtail import LogtailHandler
+import logging
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+logger.handlers = []
+
+if betterstack:
+    print("Betterstack is enabled")
+    handler = LogtailHandler(source_token=betterstack_flask_key)
+    logger.addHandler(handler)
+
+
 def successfully(message):
     rich.print(f"[bold green]✔[/bold green] {message}")
+    logger.info(f"✔ {message}")
 
 def failed(message):
     rich.print(f"[bold red]✘[/bold red] {message}")
+    logger.error(f"✘ {message}")
 
 def info(message):
     rich.print(f"[bold blue]ℹ[/bold blue] {message}")
+    logger.info(f"ℹ {message}")
 
 def warning(message):
     rich.print(f"[bold yellow]⚠[/bold yellow] {message}")
+    logger.warning(f"⚠ {message}")
 
 
 def debug(message):
     if debugging:
         rich.print(f"[bold gray]-[/bold gray] {message}")
+        logger.debug(f"- {message}")
 
     else:
         pass
