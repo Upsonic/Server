24f632092031c93b08a8e0335ebae3a67bc60c67
Onur ULUSOY <atadogan06@gmail.com>
2024-03-13 22:17:56 +0300

    Added openai api key serve mechanism (#93)

--- upsonic_on_prem/utils/accesskey.py
+++ upsonic_on_prem/utils/accesskey.py
@@ -113,7 +113,16 @@ class AccessKey:
         return self._set(self.key + ":scopes_read", [])
 
 
+    def set_openai_api_key(self, openai_api_key):
+        return self._set(self.key + ":openai_api_key", openai_api_key)
 
+    @property
+    def openai_api_key(self):
+        return self._get(self.key + ":openai_api_key")
+
+
+    def delete_openai_api_key(self):
+        return self._delete(self.key + ":openai_api_key")
 
     @property
     def scopes_write(self):
@@ -179,6 +188,7 @@ class AccessKey:
         self._delete(self.key + ":enable")
         self._delete(self.key + ":robust")
         self._delete(self.key + ":events")
+        self._delete(self.key + ":openai_api_key")
 
 
     
