a68310f47772ff8c7110831ae8de611c9f82fab2
Restyled.io <commits@restyled.io>
2024-05-24 14:45:17 +0000

    Restyled by isort

--- upsonic_on_prem/api/operations/user.py
+++ upsonic_on_prem/api/operations/user.py
@@ -1,27 +1,17 @@
-import hashlib
-from upsonic_on_prem.api import app
-
-from upsonic_on_prem.api.urls import *
-
-from upsonic_on_prem.api.utils import (
-    storage,
-    storage_2,
-    storage_4,
-    AccessKey,
-    Scope,
-    AI,
-)
-
-from upsonic_on_prem.api.utils.configs import openai_api_key
-
-from upsonic_on_prem.api.utils.github_sync import github
-
-from flask import jsonify
-from flask import request
 import base64
+import hashlib
 import time
 import traceback
+
 import requests
+from flask import jsonify, request
+
+from upsonic_on_prem.api import app
+from upsonic_on_prem.api.urls import *
+from upsonic_on_prem.api.utils import (AI, AccessKey, Scope, storage,
+                                       storage_2, storage_4)
+from upsonic_on_prem.api.utils.configs import openai_api_key
+from upsonic_on_prem.api.utils.github_sync import github
 
 
 def forward_request_to_openai_ollama(path, method, headers, data):
