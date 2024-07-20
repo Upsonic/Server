cff1edd4d6ee82f339b38dc110e46f7711d476c0
Onur ULUSOY <atadogan06@gmail.com>
2024-07-20 22:17:48 +0000

    feat: Added easy add API system

---
+++ upsonic_on_prem/api/endpoints/utils.py
@@ -0,0 +1,31 @@
+import os
+import inspect
+
+from flask import request
+
+from upsonic_on_prem.api.app import app, jsonify
+
+def get_current_directory_name():
+    # Get the calling frame
+    frame = inspect.stack()[1]
+    # Get the file path of the calling script
+    caller_file_path = frame.filename
+    
+    the_return = None
+
+    if caller_file_path:
+        # Get the directory path of the calling file
+        caller_dir = os.path.dirname(caller_file_path)
+        # Return the base name of the directory path
+        the_return = os.path.basename(caller_dir)
+    
+
+    if the_return is not None:
+        the_return = "/" + the_return.replace("_", "/")
+
+    return the_return
+
+
+def get_scope_name(request_):
+    scope = request_.form.get("scope")
+    return scope
