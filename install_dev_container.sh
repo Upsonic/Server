4aae52924d5bc82f553d216b47808ec1e37fa8cc
Onur ULUSOY <atadogan06@gmail.com>
2024-07-21 11:31:03 +0000

    fix: Fixed permission command on install_dev_container.sh

--- install_dev_container.sh
+++ install_dev_container.sh
@@ -3,4 +3,4 @@ sudo apt-get upgrade -y
 sudo apt-get install redis git gh python3 python3-pip -y
 sudo pip3 install -r requirements.txt
 sudo mkdir /db
-chown -R redis:redis /db
+sudo chown -R redis:redis /db
