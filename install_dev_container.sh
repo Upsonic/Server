cb6d1247141bdd6df5394582ce3893e16041a77f
Onur ULUSOY <atadogan06@gmail.com>
2024-08-15 18:35:36 +0000

    feat: Update dev container configuration and add environment variables

--- install_dev_container.sh
+++ install_dev_container.sh
@@ -4,3 +4,25 @@ sudo apt-get install redis git gh python3 python3-pip -y
 sudo pip3 install -r requirements.txt
 sudo mkdir /db
 sudo chown -R redis:redis /db
+
+cd upsonic_on_prem
+cd dash
+python3 manage.py migrate --noinput
+echo "from app import models; models.User.objects.create_superuser('user', 'user@upsonic.co', '123', access_key='123')" | python3 manage.py shell
+
+echo "
+admin_pass = 123
+admin_key = 123
+admin_username = user
+debug = True
+" > .env
+
+cd ..
+cd api
+
+echo "
+admin_pass = 123
+admin_key = 123
+admin_username = user
+debug = True
+" > .env
