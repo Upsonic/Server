aa32bf7123350e097ee19ac5ccd5b26971cb84e4
Onur ULUSOY <atadogan06@gmail.com>
2024-08-16 14:44:43 +0000

    feat: Added kot_db system

---
+++ upsonic_on_prem/api/utils/kot_db.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+load_dotenv(dotenv_path=".env")
+
+
+from kot import KOT
+
+kot_db_ = KOT("api")
+
+
+if not kot_db_.get("default_model"):
+    kot_db_.set("default_model" , os.environ.get("default_model", "upsonic_local_model"))
+
+
+kot_db = kot_db_
