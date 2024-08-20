e6862ecaca12ab17fe3886ddea410a55d415f17c
Onur ULUSOY <atadogan06@gmail.com>
2024-08-20 14:00:26 +0000

    chore: Update Dockerfile paths for server builds

--- run.sh
+++ run.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
 chown -R redis:redis /db
-cd /app/On-Prem/
+cd /app/Server/
 ollama serve &
 sleep 30s
 python3 ollama_setting.py
 ollama create upsonic_local_model -f Modelfile
 ollama create nomic-embed-text-upsonic -f Modelfile_2
 
-python3 /app/On-Prem/ssl_setting.py
+python3 /app/Server/ssl_setting.py
 
 service nginx start 
 
 
-cd /app/On-Prem/upsonic_on_prem/api/
+cd /app/Server/upsonic_on_prem/api/
 python3 main.py --host=0.0.0.0 --port=3000 &
 
-python3 /app/On-Prem/upsonic_on_prem/dash/manage.py makemigrations --noinput
-python3 /app/On-Prem/upsonic_on_prem/dash/manage.py migrate --noinput
-python3 /app/On-Prem/upsonic_on_prem/dash/manage.py collectstatic --noinput
+python3 /app/Server/upsonic_on_prem/dash/manage.py makemigrations --noinput
+python3 /app/Server/upsonic_on_prem/dash/manage.py migrate --noinput
+python3 /app/Server/upsonic_on_prem/dash/manage.py collectstatic --noinput
 
 
-cd /app/On-Prem/upsonic_on_prem/dash/
+cd /app/Server/upsonic_on_prem/dash/
 
 echo "from app import models; models.User.objects.create_superuser('$admin_username', 'onprem@upsonic.co', '$admin_pass', access_key='$admin_key')" | python3 manage.py shell
 
--- run_without_models.sh
+++ run_without_models.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 chown -R redis:redis /db
-cd /app/On-Prem/
+cd /app/Server/
 
-python3 /app/On-Prem/ssl_setting.py
+python3 /app/Server/ssl_setting.py
 
 service nginx start 
 
 
-cd /app/On-Prem/upsonic_on_prem/api/
+cd /app/Server/upsonic_on_prem/api/
 python3 main.py --host=0.0.0.0 --port=3000 &
 
-python3 /app/On-Prem/upsonic_on_prem/dash/manage.py makemigrations --noinput
-python3 /app/On-Prem/upsonic_on_prem/dash/manage.py migrate --noinput
-python3 /app/On-Prem/upsonic_on_prem/dash/manage.py collectstatic --noinput
+python3 /app/Server/upsonic_on_prem/dash/manage.py makemigrations --noinput
+python3 /app/Server/upsonic_on_prem/dash/manage.py migrate --noinput
+python3 /app/Server/upsonic_on_prem/dash/manage.py collectstatic --noinput
 
 
-cd /app/On-Prem/upsonic_on_prem/dash/
+cd /app/Server/upsonic_on_prem/dash/
 
 echo "from app import models; models.User.objects.create_superuser('$admin_username', 'onprem@upsonic.co', '$admin_pass', access_key='$admin_key')" | python3 manage.py shell
 
