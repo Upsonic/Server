f20414c913351384cbf9b01b7b8c83b28f0aacaf
Onur ULUSOY <atadogan06@gmail.com>
2024-08-22 07:49:57 +0000

    chore: Update diagnostic endpoint to include additional system information

--- upsonic_on_prem/api/utils/diagnostic/diagnostic.py
+++ upsonic_on_prem/api/utils/diagnostic/diagnostic.py
@@ -1,5 +1,18 @@
+import time
+
 from upsonic_on_prem.__init__ import __version__
 from upsonic_on_prem.api.utils import storage
+from upsonic_on_prem.api.utils.accesskey import AccessKey
+from upsonic_on_prem.api.utils.scope import Scope
+
+start_time = time.time()
+
+def uptime():
+    # Return the time since the server started
+    return time.time() - start_time
+
+
+
 
 
 def diagnostic():
@@ -7,4 +20,8 @@ def diagnostic():
     return {
         "version": str(__version__),
         "storage_total_size": storage.total_size(),
+        "storage_redis_status": storage.status(),
+        "total_user": AccessKey.get_len_of_users(),
+        "total_scopes": Scope.get_all_scopes_len(),
+        "uptime": uptime(),
     }
