d61c06c0d4d15eee3e1eb4749b8bebb2e8a0ed0d
Onur ULUSOY <atadogan06@gmail.com>
2024-09-07 13:27:59 +0000

    Added ai test to diagnostic page

--- upsonic_on_prem/dash/app/pages/diagnostic/page.py
+++ upsonic_on_prem/dash/app/pages/diagnostic/page.py
@@ -15,16 +15,27 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def view(request):
-
+    ai_result = None
     result = API_Integration(request.user.access_key).diagnostic()
 
-    
+
+    if request.method == "POST":
+
+        ai_prompt_ = request.POST.get("ai_prompt")
+
+
+
+        if ai_prompt_:
+            ai_result = API_Integration(request.user.access_key).ai_completion(ai_prompt_)
+            logger.info(ai_result)
+
+
         
 
 
     data = {
         "page_title": name,
-
+        "ai_result": ai_result,
         "result": result,
     }
     return render(request, f"pages/{location}/template.html", data)
