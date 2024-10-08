d620085b1f9ccd42d111062b178bf38cf51b6969
Onur ULUSOY <atadogan06@gmail.com>
2024-10-08 19:14:26 +0000

    fix: Trying to fix

--- upsonic_on_prem/api/app.py
+++ upsonic_on_prem/api/app.py
@@ -26,8 +26,25 @@ database_name_caches = []
 key_name_caches = []
 
 
-app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 
+class ReverseProxied(object):
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        script_name = environ.get('HTTP_X_SCRIPT_NAME', '')
+        if script_name:
+            environ['SCRIPT_NAME'] = script_name
+            path_info = environ['PATH_INFO']
+            if path_info.startswith(script_name):
+                environ['PATH_INFO'] = path_info[len(script_name):]
+
+        scheme = environ.get('HTTP_X_SCHEME', '')
+        if scheme:
+            environ['wsgi.url_scheme'] = scheme
+        return self.app(environ, start_response)
+
+app.wsgi_app = ReverseProxied(app.wsgi_app)
 
 from .tracer import provider
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
